82dc1288d33a4e6ac6ecddb9fb74d7deb757364f
Caylee Claww <ninjahead@gmail.com>
2023-09-18 12:04:23 -0400

    added third-party JS, partially worked

--- src/app/posts/first-post/page.js
+++ src/app/posts/first-post/page.js
@@ -1,18 +1,26 @@
-import Link from 'next/link';
-import Head from 'next/head';
-
+import Link from "next/link";
+// import Head from "next/head";
+import Script from 'next/script';
 
 // The <Head> </Head> tag does NOT work any longer - must be the below metadata object instead for each page
 export const metadata = {
-  title: 'First Post',
-  description: 'The first post of Autoimmune Eats',
-}
+  title: "First Post",
+  description: "The first post of Autoimmune Eats",
+};
 
+// The onLoad prop of Script does not work - throws following error: "Event handlers cannot be passed to Client Component props." More in notes. 
 export default function FirstPost() {
   return (
     <>
-      {/* <Head> */} 
-        <title>First Post</title>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        // onLoad={() =>
+          // console.log(`script loaded correctly, window.FB has been populated`)
+        // }
+      />
+      {/* <Head> */}
+      <title>First Post</title>
       {/* </Head> */}
       <h1>First Post</h1>
       <h2>
