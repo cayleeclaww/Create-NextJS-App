b51d5bd53ed09b95a4a2523ac1246815137cca87
Caylee Claww <ninjahead@gmail.com>
2023-09-18 10:12:04 -0400

    Learning and added metadata object

--- src/app/posts/first-post/page.js
+++ src/app/posts/first-post/page.js
@@ -1,11 +1,22 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
+
+// The <Head> </Head> tag does NOT work any longer - must be the below metadata object instead for each page
+export const metadata = {
+  title: 'First Post',
+  description: 'The first post of Autoimmune Eats',
+}
 
 export default function FirstPost() {
   return (
     <>
+      {/* <Head> */} 
+        <title>First Post</title>
+      {/* </Head> */}
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to home</Link>
+        <Link href="/">← Back to home</Link>
       </h2>
     </>
   );
