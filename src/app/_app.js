8c26ea2f0f5cea6463f783a88694b9da0858d196
Caylee Claww <ninjahead@gmail.com>
2023-09-18 13:24:29 -0400

    Add CSS styles, including global styles and polishing the layout

---
+++ src/app/_app.js
@@ -0,0 +1,9 @@
+//This doesn't appear to affect anything, so I commented it out 
+
+
+
+// import '../global.css';
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
--- src/app/posts/first-post/page.js
+++ src/app/posts/first-post/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 // import Head from "next/head";
 import Script from 'next/script';
+import Layout from '../../components/layout'; //Using CSS Modules like this creates unique container names, so there are never any naming conflicts 
 
 // The <Head> </Head> tag does NOT work any longer - must be the below metadata object instead for each page
 export const metadata = {
@@ -11,7 +12,7 @@ export const metadata = {
 // The onLoad prop of Script does not work - throws following error: "Event handlers cannot be passed to Client Component props." More in notes. 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
         strategy="lazyOnload"
@@ -23,9 +24,10 @@ export default function FirstPost() {
       <title>First Post</title>
       {/* </Head> */}
       <h1>First Post</h1>
-      <h2>
+{/* Below no longer needed, as utils.module.css now contains this code  */}
+      {/* <h2>
         <Link href="/">← Back to home</Link>
-      </h2>
-    </>
+      </h2> */}
+    </Layout>
   );
 }
